743eb0499a882195fc012c908b53331fda520078
Davi Dantas <davi.soares02@gmail.com>
2023-05-24 15:42:40 -0300

    adding r-pentomino starter

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -3,11 +3,11 @@
     <section class='w-fit m-auto'>
         <Navbar/>
         
-        <div class="bg-neutral-50 rounded">
+        <div class="bg-neutral-900 rounded">
             
             <div v-for="(row, i) in grid" :key="i" class = 'flex'>
                 <div @click = "grid[i][j] ^= 1" @mouseover = "interaction($event, i, j)"  v-for="(value, j) in row" :key="j"
-                 class = 'cell border border-neutral-200 ' :class = "{'bg-neutral-900' : grid[i][j]}"></div>
+                 class = 'cell border border-neutral-800 ' :class = "{'bg-neutral-200' : grid[i][j]}"></div>
             </div>       
         </div>
 
@@ -16,8 +16,10 @@
             <button v-if="!isActive" @click = "toggle">Start <font-awesome-icon :icon="['fas', 'play']"/></button>
             <button v-else @click = toggle>Pause <font-awesome-icon :icon="['fas', 'pause']" /> </button>
             <button @click = "randomGrid">Randomize <font-awesome-icon :icon="['fas', 'shuffle']" /></button>
-            <button @click = "clear">Clear Grid <font-awesome-icon :icon="['fas', 'eraser']" /></button>
+            <button @click = "rPentomino">R-Pentomino</button>
             <Oscilators :createMatrix='createMatrix' :cols='cols' :rows='rows' @structure='oscilator => grid = oscilator.matrix'/>
+            <button @click = "clear">Clear Grid <font-awesome-icon :icon="['fas', 'eraser']" /></button>
+            <!-- <button @click='log(grid)'>log</button> -->
 
         </div>
 
@@ -37,115 +39,150 @@ library.add(faPlay, faPause, faShuffle, faEraser)
 const cols = 50
 const rows = 100
 
-const createMatrix = (cols, rows) => {
-    let matrix = new Array(cols).fill().map(() => Array(rows).fill(0))
-    return matrix
+const rPentomino = () => {
+    const rPentominoData = [
+    [
+        22,
+        46
+    ],
+    [
+        22,
+        47
+    ],
+    [
+        23,
+        45
+    ],
+    [
+        23,
+        46
+    ],
+    [
+        24,
+        46
+    ]
+]
+    rPentominoData.forEach(coords => {
+        grid.value[coords[0]][coords[1]] = 1
+    });
 }
 
-const interaction = (e, i, j) => {
-    if (e.buttons == 1 || e.buttons == 3) {
-        grid.value[i][j] ^= 1
+const log = (matrix) => {
+    let coords = []
+    for (let i = 0; i < cols; i++) 
+        for (let j = 0; j < rows; j++)
+            if (matrix[i][j]) 
+                coords.push([i, j])
+    
+    console.log(coords)
+}
+    const createMatrix = (cols, rows) => {
+        let matrix = new Array(cols).fill().map(() => Array(rows).fill(0))
+        return matrix
     }
-}
 
-const clear = () => {
-    grid.value = createMatrix(cols, rows)
-    isActive.value = false
-}   
-
- 
-
-const livingNeighbors = (matrix, i, j) => {
-    let living = 0
-
-    //Make edges linked to its opposite 
-    living += matrix[(i + cols) % cols][(j + 1 + rows) % rows]
-    living += matrix[(i + cols) % cols][(j - 1 + rows) % rows]
-    living += matrix[(i + 1 + cols) % cols][(j + 1 + rows) % rows]
-    living += matrix[(i + 1 + cols) % cols][(j - 1 + rows) % rows]
-    living += matrix[(i + 1 + cols) % cols][(j + rows) % rows]
-    living += matrix[(i - 1 + cols) % cols][(j + 1 + rows) % rows]
-    living += matrix[(i - 1 + cols) % cols][(j - 1 + rows) % rows]
-    living += matrix[(i - 1 + cols) % cols][(j + rows) % rows]
-
-    return living
-}
-
-const randomGrid = () => {
-
-    let newGrid = createMatrix(cols, rows)
-
-    newGrid = newGrid.map(row => {
-        row = row.map(() => Math.round(Math.random()))
-        return row
-    })
-
-    grid.value = newGrid
-}
-
-let matrix = createMatrix(cols, rows)
-
-let grid = ref(matrix)
-let isActive = ref(false)
-
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-const toggle = () => {
-    isActive.value = !isActive.value
-}
-
-const update = async () => {
-
-    while (isActive.value != false) {
-        await sleep(2)
-        grid.value = updateGrid(grid.value)
-    }
-}
-watch(isActive, update)
-
-function get_positions(matrix){
-    let pos = []
-    for(let i = 0; i < cols; i++){
-        for(let j = 0; j < rows; j++){
-            if (matrix[i][j] == 1){
-                pos.push([i,j])
-            }
+    const interaction = (e, i, j) => {
+        if (e.buttons == 1 || e.buttons == 3) {
+            grid.value[i][j] ^= 1
         }
     }
 
-   console.log(pos)
-} 
-
-const updateGrid = (prev) => {
-    let descendant = createMatrix(cols, rows)
-
-    for (let i = 0; i < cols; i++) {
-        for (let j = 0; j < rows; j++) {
-            const living = livingNeighbors(prev, i, j)
-            const status = prev[i][j]
-
-            if (status == 0 && living == 3) {
-
-                descendant[i][j] = 1
-            }
-
-            else if (status == 1 && (living < 2 || living > 3)) {
-
-                descendant[i][j] = 0
-            }
-
-            else {
-
-                descendant[i][j] = status
-            }
-        }
+    const clear = () => {
+        grid.value = createMatrix(cols, rows)
+        isActive.value = false
     }
 
+    const livingNeighbors = (matrix, i, j) => {
+        let living = 0
 
-    return descendant
-}
+        //Make edges linked to its opposite 
+        living += matrix[(i + cols) % cols][(j + 1 + rows) % rows]
+        living += matrix[(i + cols) % cols][(j - 1 + rows) % rows]
+        living += matrix[(i + 1 + cols) % cols][(j + 1 + rows) % rows]
+        living += matrix[(i + 1 + cols) % cols][(j - 1 + rows) % rows]
+        living += matrix[(i + 1 + cols) % cols][(j + rows) % rows]
+        living += matrix[(i - 1 + cols) % cols][(j + 1 + rows) % rows]
+        living += matrix[(i - 1 + cols) % cols][(j - 1 + rows) % rows]
+        living += matrix[(i - 1 + cols) % cols][(j + rows) % rows]
+
+        return living
+    }
+
+    const randomGrid = () => {
+
+        let newGrid = createMatrix(cols, rows)
+
+        newGrid = newGrid.map(row => {
+            row = row.map(() => Math.round(Math.random()))
+            return row
+        })
+
+        grid.value = newGrid
+    }
+
+    let matrix = createMatrix(cols, rows)
+
+    let grid = ref(matrix)
+    let isActive = ref(false)
+
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    const toggle = () => {
+        isActive.value = !isActive.value
+    }
+
+    const update = async () => {
+
+        while (isActive.value != false) {
+            await sleep(2)
+            grid.value = updateGrid(grid.value)
+        }
+    }
+    watch(isActive, update)
+
+    function get_positions(matrix) {
+        let pos = []
+        for (let i = 0; i < cols; i++) {
+            for (let j = 0; j < rows; j++) {
+                if (matrix[i][j] == 1) {
+                    pos.push([i, j])
+                }
+            }
+        }
+
+        console.log(pos)
+    }
+
+    const updateGrid = (prev) => {
+        let descendant = createMatrix(cols, rows)
+
+        for (let i = 0; i < cols; i++) {
+            for (let j = 0; j < rows; j++) {
+                const living = livingNeighbors(prev, i, j)
+                const status = prev[i][j]
+
+                if (status == 0 && living == 3) {
+
+                    descendant[i][j] = 1
+                }
+
+                else if (status == 1 && (living < 2 || living > 3)) {
+
+                    descendant[i][j] = 0
+                }
+
+                else {
+
+                    descendant[i][j] = status
+                }
+            }
+        }
+
+
+        return descendant
+    }
 
 
 
@@ -157,5 +194,4 @@ const updateGrid = (prev) => {
     width: 1rem;
     height: 1rem;
 }
-
 </style>
